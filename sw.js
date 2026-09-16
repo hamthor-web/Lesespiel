0ad619b8c04b6bf6d2bd8cfe9aeaa4d2ff436fc0
hamthor-web <h.amthor@gmail.com>
2026-09-17 00:14:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "interaktive-geschichten-v1";
+const CACHE_NAME = "interaktive-geschichten-v2";
 const CORE_FILES = [
   "./index.html",
   "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icon-192.png?v=2",
+  "./icon-512.png?v=2"
 ];
 
 self.addEventListener("install", event => {
